dc2cdd7840acc61864f6eea5b0a5c4f50bcfdc23
Alexin <alexindevs@gmail.com>
2024-06-12 02:00:27 +0100

    Update src/authentication/authentication.dto.ts

--- src/authentication/authentication.dto.ts
+++ src/authentication/authentication.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsBoolean, IsEmail, IsNotEmpty } from 'class-validator';
+import { IsString, IsEmail, IsNotEmpty, Length } from 'class-validator';
 
 export class CreateAccountDto {
   @IsEmail()
@@ -7,16 +7,61 @@ export class CreateAccountDto {
 
   @IsString()
   @IsNotEmpty()
+  @Length(6, 20)
   password: string;
 
   @IsString()
   @IsNotEmpty()
+  @Length(3, 20)
   username: string;
 
   @IsString()
   @IsNotEmpty()
   role: string;
+}
 
-  @IsBoolean()
-  verified: boolean;
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20)
+  password: string;
+}
+
+export class VerifyEmailDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+}
+
+export class VerifyUserDto {
+  @IsString()
+  @IsNotEmpty()
+  token: string;
+}
+
+export class StartPasswordResetDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+}
+
+export class ResetPasswordDto {
+  @IsString()
+  @IsNotEmpty()
+  token: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20)
+  password: string;
+}
+
+export class LogoutDto {
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
 }
