a6529a587c288b940767f36c09bc2e6d77a99c2d
Alexin <alexindevs@gmail.com>
2024-06-12 02:00:27 +0100

    Update src/authentication/authentication.module.ts

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { EmailsService } from '../emails/emails.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AccountSchema, RefreshTokenSchema } from './authentication.schema';
+import { OtpModule } from 'src/otp/otp.module';
+import { AccessTokenService } from './accesstoken.service';
+import { RefreshTokenService } from './refreshtoken.service';
 
 @Module({
-  providers: [AuthenticationService, EmailsService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Account',
+        schema: AccountSchema,
+      },
+      {
+        name: 'RefreshToken',
+        schema: RefreshTokenSchema,
+      },
+    ]),
+    OtpModule,
+  ],
+  providers: [
+    AuthenticationService,
+    AccessTokenService,
+    RefreshTokenService,
+    EmailsService,
+  ],
   controllers: [AuthenticationController],
 })
 export class AuthenticationModule {}
