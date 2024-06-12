c7b42456fae5994b99db049a18ab52e6ba32eb3d
Alexin <alexindevs@gmail.com>
2024-06-12 02:00:27 +0100

    Update src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/dorm-haven'),
     AuthenticationModule,
+    ProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
